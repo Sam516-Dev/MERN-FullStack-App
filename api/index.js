6bd81bee7d5b75f739c9512462651a3ab374d394
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2024-01-22 23:08:24 +0300

    added some routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,14 @@ import express  from "express";
 
 const app = express()
 
+
+app.get("/login", (req,res)=>{
+res.send("You tried to login !")
+})
+
+
+
+
 app.listen(3000, ()=>{
     console.log("app listening on port 3000 !!")
 })
