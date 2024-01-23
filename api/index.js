827c582e6ba250f8991374ea28d65a81fbf72a57
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2024-01-24 00:57:30 +0300

    Mongo db connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGOPASS).then(()=>{
+    console.log('connected to mongo db ...')
+}).catch((err)=>{
+    console.log(err)
+})
+    
 
 const app = express()
 
