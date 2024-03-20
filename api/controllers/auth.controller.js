8f7d6bb7cd25043ca8f958cbe5800921f33dfe90
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2024-03-21 00:14:37 +0300

    error middlewares added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next ) => {
   const { username, email, password } = req.body;
   const hashedpassword = bcryptjs.hashSync(password, 10)
   const newUser = new User({ username, email, password:hashedpassword });
@@ -10,6 +10,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("user created successifully ! ");
   } catch (error) {
-    res.status(500).json(error.message)
+    next(error)
   }
 };
--- api/index.js
+++ api/index.js
@@ -22,6 +22,18 @@ app.use(express.json())
 app.use("/api/user", userRounter)
 app.use("/api/auth", authRouter)
 
+
+app.use((err, req, res, next)=>{
+const statusCode = err.statusCode || 500
+const message = err.message || "Internal server error"
+return res.status(statusCode).json({
+  success: false,
+  statusCode,
+  message
+})
+})
+
+
 app.listen(3000, () => {
   console.log("app listening on port 3000 !!");
 });
