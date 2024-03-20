878f86cffa16a8bb00967c914616ee2e1a3c34a4
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2024-03-20 23:55:24 +0300

    sign-up api done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"
+
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("user created successifully ! ");
+  const hashedpassword = bcryptjs.hashSync(password, 10)
+  const newUser = new User({ username, email, password:hashedpassword });
+  try {
+    await newUser.save();
+    res.status(201).json("user created successifully ! ");
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
 };
