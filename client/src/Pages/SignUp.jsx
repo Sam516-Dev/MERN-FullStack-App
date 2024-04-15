a0438a3ed489ae1df73a0665b21a547885ab4418
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2024-04-15 13:45:50 +0300

    submit formData to server

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,30 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+
+
 const SignUp = () => {
+
+const [formData, setformData] = useState({});
+
+const handleChange = (e) => {
+  setformData({
+    ...formData,
+    [e.target.id]: e.target.value,
+  });
+};
+console.log(formData)
+
+const handleSubmit = async (e) =>{
+  e.preventDefault()
+  const res = await fetch("/api/auth/signup",{
+    method: "POST",
+    headers:{
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  )
+  const data = await res.json()
+  console.log(data)
+}
+
+
   return (
     <div className="p-3 max-w-lg mx-auto bg-indigo-200 mt-9 rounded-md py-16 px-6">
       <h1 className="text-3xl text-center font-bold mb-8 text-indigo-950 ">
         Sign Up
       </h1>
-      <form className="flex flex-col gap-6">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
         <input
           type="text"
           placeholder="username"
           className="text-indigo-950  outline-none font-medium border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="text-indigo-950  outline-none font-medium border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className=" text-indigo-950 outline-none font-medium border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
         <button className="bg-indigo-950 py-4 mt-6 rounded-md text-white font-bold text-base uppercase hover:bg-indigo-800">
           Sign Up
