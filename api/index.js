48aa2d648cfb2e85091f019fda46a6fe0eee8641
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2024-07-08 23:27:33 +0300

    updates

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRounter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
+import userRounter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -12,27 +12,24 @@ mongoose
     console.log("connected to mongo db ...");
   })
   .catch((err) => {
-    console.log(err);
+    console.log(" here is the error from mongoose:", err);
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api/user", userRounter);
+app.use("/api/auth", authRouter);
 
-app.use("/api/user", userRounter)
-app.use("/api/auth", authRouter)
-
-
-app.use((err, req, res, next)=>{
-const statusCode = err.statusCode || 500
-const message = err.message || "Internal server error"
-return res.status(statusCode).json({
-  success: false,
-  statusCode,
-  message
-})
-})
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("app listening on port 3000 !!");
